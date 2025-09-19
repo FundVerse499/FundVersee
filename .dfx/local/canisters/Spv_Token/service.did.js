ff4d32221b5c1f6f11657f600bb06c12fe067739
MarwanIssa100 <missa4312@gmail.com>
2025-09-19 18:36:05 +0300

    commit changes

--- .dfx/local/canisters/Spv_Token/service.did.js
+++
@@ -1,7 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'balance_of' : IDL.Func([IDL.Principal], [IDL.Nat64], ['query']),
-    'mint' : IDL.Func([IDL.Principal, IDL.Nat64], [], []),
-  });
-};
-export const init = ({ IDL }) => { return []; };
